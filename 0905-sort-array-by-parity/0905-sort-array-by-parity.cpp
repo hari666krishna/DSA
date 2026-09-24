0cd4ae016725acf3f5edf9c4986995feddf4890d
Hari Krishna <harikrishnasanneboina666@gmail.com>
2026-09-24 22:48:22 +0530

    Time: 1 ms (18.07%), Space: 20.8 MB (22.22%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int>final;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                final.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] % 2!=0){
+                final.push_back(nums[i]);
+            }
+        }
+        return final;
+    }
+};
